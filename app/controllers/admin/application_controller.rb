fa67b9ce853efb3a64c8c388b42452a64c271984
Kushagra <Kushagra.srivastava@w3villa.com>
2021-06-24 11:18:17 +0530

    polymorphic

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,17 @@
+class Admin::ApplicationController < ApplicationController
+	# layout "welcome"
+	before_action :authenticate_user!
+	before_action :authorize_user
+	layout "dashboard"
+
+	private
+
+		def authorize_user
+			if  current_user.role != "ADMIN"
+				redirect_to root_path, notice: "You are not authorized to access this panel"
+			end
+
+		end
+end
+
+  
---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :hobbies
+  resources :blog_tags
+  resources :tags
+  resources :categories
+  devise_for :users
+  resources :blogs 
+  get "blogs/categories/:category_slug", to: "blogs#show_category", as: :show_blogs_category
+  get "blogs/tags/:id", to: "blogs#show_tag", as: :blogs_tags
+  root to: "blogs#index"
+
+  resources :blogs do
+      resources :comments
+  end
+
+  resources :comments do
+      resources :comments
+  end
+  # get "blogs/new", to: "blogs#new"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/users"
+  namespace :admin do 
+    root to: "dashboard#index"
+    resources :users
+  end
+end
